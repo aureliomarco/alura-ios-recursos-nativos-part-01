fe96bc94b0a2d3a2e8e14e25166e80aa378af591
Marco Aurélio Sanches Gonçalves <marco182.2@gmail.com>
2020-04-20 16:22:16 -0300

    [AULA-01] Utilizando a biblioteca de fotos

--- Agenda/Componentes/ImagePicker.swift
+++ Agenda/Componentes/ImagePicker.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum MenuOpcoes {
+    case camera
+    case biblioteca
+}
+
 protocol ImagePickerFotoSelecionada {
     func imagePickerFotoSelecionada(_ foto: UIImage)
 }
@@ -24,5 +29,24 @@ class ImagePicker: NSObject, UIImagePickerControllerDelegate, UINavigationContro
         let foto = info[UIImagePickerControllerOriginalImage] as! UIImage
         delegate?.imagePickerFotoSelecionada(foto)
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    // Menu ActionSheet 💩💩💩💩💩
+    func menuDeOpcoes(completion:@escaping(_ opcao: MenuOpcoes) -> Void) -> UIAlertController {
+        let menu = UIAlertController(title: "Atenção", message: "Escolha uma das opções abaixo", preferredStyle: .actionSheet)
+        let camera = UIAlertAction(title: "Tirar foto", style: .default) { (acao) in
+            completion(.camera)
+        }
+        menu.addAction(camera)
+        
+        let biblioteca = UIAlertAction(title: "Biblioteca", style: .default) { (acao) in
+            completion(.biblioteca)
+        }
+        menu.addAction(biblioteca)
+        
+        let cancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        menu.addAction(cancelar)
+        
+        return menu
     }
 }
